10ff2aa33604c4b00fbbd231a2b582781256fc37
Juyeon <remake867@gmail.com>
2021-11-29 22:19:57 +0900

    etc: foldering

--- AppStoreClone/AppSummaryTableViewCell.swift
+++ AppStoreClone/AppSummaryTableViewCell.swift
@@ -9,19 +9,7 @@ import UIKit
 
 class AppSummaryTableViewCell: UITableViewCell {
     @IBOutlet weak var collectionView: UICollectionView!
-    var appSummaryViewController: AppSummaryViewController?
     override func awakeFromNib() {
         super.awakeFromNib()
-        collectionView.dataSource = self
-    }
-}
-
-extension AppSummaryTableViewCell: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        5
     }
 }
