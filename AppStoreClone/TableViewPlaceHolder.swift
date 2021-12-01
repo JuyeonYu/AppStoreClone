a4ee377dac64e496be369ac09c961120da8fab9a
JuYeonYu <remake867@gmail.com>
2021-12-02 01:39:14 +0900

    bug: init placeHolder

--- AppStoreClone/TableViewPlaceHolder.swift
+++ AppStoreClone/TableViewPlaceHolder.swift
@@ -17,6 +17,6 @@ class TableViewPlaceHolder: UIView {
         super.init(frame: frame)
     }    
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 }
